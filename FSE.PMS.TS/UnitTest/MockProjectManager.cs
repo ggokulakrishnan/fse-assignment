494da1b80d0e7cd8e662486bf3c29601e1d86100
Gokul, G <gokul.g@hotmail.com>
2019-12-17 18:31:11 +0530

    Fixing error in NSubstitute mock EF framework

--- FSE.PMS.TS/UnitTest/MockProjectManager.cs
+++ FSE.PMS.TS/UnitTest/MockProjectManager.cs
@@ -36,13 +36,19 @@ namespace FSE.PMS.Tests.UnitTest
                     StartDate=DateTime.Now.Date,
                     EndDate=DateTime.Now.Date.AddDays(1)
                 }
-        }.AsQueryable();
+            }.AsQueryable();
 
-            IDbSet<Projects> mocksetProjects = Substitute.For<IDbSet<Projects>>();
-            mocksetProjects.Provider.Returns(dataProjects.Provider);
-            mocksetProjects.Expression.Returns(dataProjects.Expression);
-            mocksetProjects.ElementType.Returns(dataProjects.ElementType);
-            mocksetProjects.GetEnumerator().Returns(dataProjects.GetEnumerator());
+            //IDbSet<Projects> mocksetProjects = Substitute.For<IDbSet<Projects>>();
+            //mocksetProjects.Provider.Returns(dataProjects.Provider);
+            //mocksetProjects.Expression.Returns(dataProjects.Expression);
+            //mocksetProjects.ElementType.Returns(dataProjects.ElementType);
+            //mocksetProjects.GetEnumerator().Returns(dataProjects.GetEnumerator());
+
+            var mocksetProjects = Substitute.For<DbSet<Projects>, IQueryable<Projects>>();
+            ((IQueryable<Projects>)mocksetProjects).Provider.Returns(dataProjects.Provider);
+            ((IQueryable<Projects>)mocksetProjects).Expression.Returns(dataProjects.Expression);
+            ((IQueryable<Projects>)mocksetProjects).ElementType.Returns(dataProjects.ElementType);
+            ((IQueryable<Projects>)mocksetProjects).GetEnumerator().Returns(dataProjects.GetEnumerator());
 
             var dataUsers = new List<Users>()
             {
@@ -70,14 +76,20 @@ namespace FSE.PMS.Tests.UnitTest
                     LastName="M",
                     EmployeeID="433469"
                 }
-        }.AsQueryable();
+            }.AsQueryable();
 
-            IDbSet<Users> mocksetUsers = Substitute.For<IDbSet<Users>>();
-            mocksetUsers.Provider.Returns(dataUsers.Provider);
-            mocksetUsers.Expression.Returns(dataUsers.Expression);
-            mocksetUsers.ElementType.Returns(dataUsers.ElementType);
-            mocksetUsers.GetEnumerator().Returns(dataUsers.GetEnumerator());
+            //IDbSet<Users> mocksetUsers = Substitute.For<IDbSet<Users>>();
+            //mocksetUsers.Provider.Returns(dataUsers.Provider);
+            //mocksetUsers.Expression.Returns(dataUsers.Expression);
+            //mocksetUsers.ElementType.Returns(dataUsers.ElementType);
+            //mocksetUsers.GetEnumerator().Returns(dataUsers.GetEnumerator());
 
+            var mocksetUsers = Substitute.For<DbSet<Users>, IQueryable<Users>>();
+            ((IQueryable<Users>)mocksetUsers).Provider.Returns(dataUsers.Provider);
+            ((IQueryable<Users>)mocksetUsers).Expression.Returns(dataUsers.Expression);
+            ((IQueryable<Users>)mocksetUsers).ElementType.Returns(dataUsers.ElementType);
+            ((IQueryable<Users>)mocksetUsers).GetEnumerator().Returns(dataUsers.GetEnumerator());
+            
             var dataTasks = new List<Tasks>()
             {
                 new Tasks
@@ -118,13 +130,19 @@ namespace FSE.PMS.Tests.UnitTest
                     EndDate=DateTime.Now.Date.AddDays(1),
                     Status=true
                 }
-        }.AsQueryable();
+            }.AsQueryable();
 
-            IDbSet<Tasks> mocksetTasks = Substitute.For<IDbSet<Tasks>>();
-            mocksetTasks.Provider.Returns(dataTasks.Provider);
-            mocksetTasks.Expression.Returns(dataTasks.Expression);
-            mocksetTasks.ElementType.Returns(dataTasks.ElementType);
-            mocksetTasks.GetEnumerator().Returns(dataTasks.GetEnumerator());
+            //IDbSet<Tasks> mocksetTasks = Substitute.For<IDbSet<Tasks>>();
+            //mocksetTasks.Provider.Returns(dataTasks.Provider);
+            //mocksetTasks.Expression.Returns(dataTasks.Expression);
+            //mocksetTasks.ElementType.Returns(dataTasks.ElementType);
+            //mocksetTasks.GetEnumerator().Returns(dataTasks.GetEnumerator());
+
+            var mocksetTasks = Substitute.For<DbSet<Tasks>, IQueryable<Tasks>>();
+            ((IQueryable<Tasks>)mocksetTasks).Provider.Returns(dataTasks.Provider);
+            ((IQueryable<Tasks>)mocksetTasks).Expression.Returns(dataTasks.Expression);
+            ((IQueryable<Tasks>)mocksetTasks).ElementType.Returns(dataTasks.ElementType);
+            ((IQueryable<Tasks>)mocksetTasks).GetEnumerator().Returns(dataTasks.GetEnumerator());
 
             var dataPTasks = new List<ParentTasks>()
             {
@@ -138,15 +156,27 @@ namespace FSE.PMS.Tests.UnitTest
                     ParentTaskID=2,
                     ParentTask="Parent Task 2"
                 }
-        }.AsQueryable();
+            }.AsQueryable();
 
-            IDbSet<ParentTasks> mocksetPTasks = Substitute.For<IDbSet<ParentTasks>>();
-            mocksetPTasks.Provider.Returns(dataPTasks.Provider);
-            mocksetPTasks.Expression.Returns(dataPTasks.Expression);
-            mocksetPTasks.ElementType.Returns(dataPTasks.ElementType);
-            mocksetPTasks.GetEnumerator().Returns(dataPTasks.GetEnumerator());
+            //IDbSet<ParentTasks> mocksetPTasks = Substitute.For<IDbSet<ParentTasks>>();
+            //mocksetPTasks.Provider.Returns(dataPTasks.Provider);
+            //mocksetPTasks.Expression.Returns(dataPTasks.Expression);
+            //mocksetPTasks.ElementType.Returns(dataPTasks.ElementType);
+            //mocksetPTasks.GetEnumerator().Returns(dataPTasks.GetEnumerator());
 
-            ProjectManagerEntities mockContext = Substitute.For<ProjectManagerEntities>();
+            //ProjectManagerEntities mockContext = Substitute.For<ProjectManagerEntities>();
+            //mockContext.Projects.Returns(mocksetProjects);
+            //mockContext.Users.Returns(mocksetUsers);
+            //mockContext.Tasks.Returns(mocksetTasks);
+            //mockContext.ParentTasks.Returns(mocksetPTasks);
+            
+            var mocksetPTasks = Substitute.For<DbSet<ParentTasks>, IQueryable<ParentTasks>>();
+            ((IQueryable<ParentTasks>)mocksetPTasks).Provider.Returns(dataPTasks.Provider);
+            ((IQueryable<ParentTasks>)mocksetPTasks).Expression.Returns(dataPTasks.Expression);
+            ((IQueryable<ParentTasks>)mocksetPTasks).ElementType.Returns(dataPTasks.ElementType);
+            ((IQueryable<ParentTasks>)mocksetPTasks).GetEnumerator().Returns(dataPTasks.GetEnumerator());
+
+            var mockContext = Substitute.For<ProjectManagerEntities>();
             mockContext.Projects.Returns(mocksetProjects);
             mockContext.Users.Returns(mocksetUsers);
             mockContext.Tasks.Returns(mocksetTasks);
@@ -154,6 +184,5 @@ namespace FSE.PMS.Tests.UnitTest
 
             return mockContext;
         }
-
     }
 }
